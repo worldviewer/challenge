48edba2b23fe2d3604ac6a7d425fe96e60d9e392
Chris Reeve <paradigmsareconstructed@gmail.com>
2019-05-24 06:15:14 -0700

    Removed key from repo

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,4 +1,4 @@
-const KEY = '***REMOVED***';
+const KEY = process.env.REACT_APP_OPENWEATHERMAP_KEY;
 
 export const getWeather = (lat, lng) => fetch(`https://api.openweathermap.org/data/2.5/forecast?APPID=${KEY}&lat=${lat}&lon=${lng}`)
   .then((resp) => resp.json())
